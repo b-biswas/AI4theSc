1ec41a39bff7f97a2c9d8be6a5eaab5a681bb1b1
Biswajit Biswas <bbiswas.dgp@gmail.com>
2021-11-08 17:44:58 +0100

    adding unit test

---
+++ tests/test_regression.py
@@ -0,0 +1,8 @@
+import numpy as np
+from linear_regression import linear_regression
+
+def test_dimension():
+    X = np.random.randn(3,2)
+    y = np.random.randn(3)
+    coefs = linear_regression(X,y)
+    assert len(coefs)==2
