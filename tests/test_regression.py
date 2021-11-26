041617a59da21576a4f0da27c36d7a5a8105cd91
Biswajit Biswas <bbiswas.dgp@gmail.com>
2021-11-26 19:08:03 +0100

    removed old code

--- tests/test_regression.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from linear_regression import linear_regression # for this to work you need to have run the pip install command
-
-def test_dimension():
-    X = np.random.randn(3,2)
-    y = np.random.randn(3)
-    coefs = linear_regression(X,y)
-    assert len(coefs)==2
